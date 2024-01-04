6ae6c6b7b569721fca2ee04558c3ac29dba8514c
LaneyS05 <laney.staggs05@gmail.com>
2024-01-04 09:59:58 -0500

    making it pretty

--- models/band.js
+++ models/band.js
@@ -1,55 +1,51 @@
-'use strict'
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ MeetGreet, SetTime }) {
       // meet and greets
       Band.hasMany(MeetGreet, {
         foreignKey: "band_id",
-        as: "meet_greets"
-      })
+        as: "meet_greets",
+      });
 
-      // set times 
+      // set times
       Band.hasMany(SetTime, {
         foreignKey: "band_id",
-        as: "set_times"
-      })
+        as: "set_times",
+      });
     }
   }
-  Band.init({
-    band_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
+  Band.init(
+    {
+      band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
         type: DataTypes.STRING,
-        allowNull: false
-    },
-    genre: {
+        allowNull: false,
+      },
+      genre: {
         type: DataTypes.TEXT,
-        allowNull: false
-    },
-    available_start_time: {
+        allowNull: false,
+      },
+      available_start_time: {
         type: DataTypes.DATE,
-        allowNull: false
-    },
-    end_time: {
+        allowNull: false,
+      },
+      end_time: {
         type: DataTypes.DATE,
-        allowNull: false
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Band",
+      tableName: "bands",
+      timestamps: false,
     }
-  }, {
-    sequelize,
-    modelName: 'Band',
-    tableName: 'bands',
-    timestamps: false
-  })
-  return Band
-}
+  );
+  return Band;
+};
--- models/stage.js
+++ models/stage.js
@@ -1,44 +1,40 @@
-'use strict'
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Stage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Event, StageEvent, SetTime }) {
-      // events 
+      // events
       Stage.belongsToMany(Event, {
         foreignKey: "stage_id",
         as: "events",
-        through: StageEvent
-      })
+        through: StageEvent,
+      });
 
-      // set times 
+      // set times
       Stage.hasMany(SetTime, {
         foreignKey: "stage_id",
-        as: "set_times"
-      })
+        as: "set_times",
+      });
     }
   }
-  Stage.init({
-    stage_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+  Stage.init(
+    {
+      stage_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      stage_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    stage_name: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      sequelize,
+      modelName: "Stage",
+      tableName: "stages",
+      timestamps: false,
     }
-  }, {
-    sequelize,
-    modelName: 'Stage',
-    tableName: 'stages',
-    timestamps: false
-  })
-  return Stage
-}
+  );
+  return Stage;
+};
