669281e8280e73ad0de6c4c676ba8ec6903c3913
LaneyS05 <laney.staggs05@gmail.com>
2024-01-03 22:02:52 -0500

    adding association-queries

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
-const { Sequelize } = require('sequelize')
-const bands = require('./controllers/bands_controller')
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
+const bands = require("./controllers/bands_controller");
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Tour API",
+  });
+});
 
-// CONTROLLERS  
-const bandsController = require('./controllers/bands_controller')
-app.use('/bands', bandsController)
+// CONTROLLERS
+const bandsController = require("./controllers/bands_controller");
+app.use("/bands", bandsController);
 
-const eventsController = require('./controllers/events_controller')
-app.use('/events', eventsController)
+const eventsController = require("./controllers/events_controller");
+app.use("/events", eventsController);
 
-const stagesController = require('./controllers/stages_controller')
-app.use('/stages', stagesController)
+const stagesController = require("./controllers/stages_controller");
+app.use("/stages", stagesController);
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
